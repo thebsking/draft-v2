c4c5b0aef8c3f7f934cc7ad2a337232faa8cc8b1
Brandon King <brandonscottking@gmail.com>
2022-09-12 20:39:51 -0500

    completed basics of roster routes

--- app/routes/rosters/$owner.tsx
+++ app/routes/rosters/$owner.tsx
@@ -3,32 +3,45 @@ import { useLoaderData } from "@remix-run/react";
 import type { LoaderFunction } from "@remix-run/node";
 import invariant from "tiny-invariant";
 
-import type { Team } from "~/models/team.server";
+import type { User } from "~/models/user.server";
 import type { Player } from "~/models/player.server";
-import { getTeamByUser } from "~/models/team.server";
+import type { Team } from "~/models/team.server"
+import { getUserById } from "~/models/user.server";
 import { getPlayersByFantasyTeam } from "~/models/player.server"
+import { getTeamByUser } from "~/models/team.server"
 
-type LoaderData = { team: Team, players: Player[] };
+type LoaderData = { user: User; players: Player[], team: Team };
 
 export const loader: LoaderFunction = async ({
   params,
 }) => {
-  invariant(params.userId, `params.id is required`);
+  invariant(params.owner, `params.owner is required`);
 
-  const team = await getTeamByUser(params.userId);
-  invariant(team, `Team not found: ${params.userId}`)
+  const user = await getUserById(params.owner);
+  invariant(user, `Team not found: ${params.owner}`)
+
+  const team = await getTeamByUser(user.id)
+  invariant(team, `No team found for ${user.name}`)
 
   const players = await getPlayersByFantasyTeam(team.id);
   invariant(players, `No players found for team ${team.name}`)
 
-  return json<LoaderData>({ team, players })
+  return json<LoaderData>({ user, players, team })
 };
 
 export default function UserTeam() {
-  const { players } = useLoaderData() as LoaderData;
+  const { user, team, players } = useLoaderData() as unknown as LoaderData;
   return (
-    {players.map((x,i) => {
-      <p key={i}>{x.name}</p>
-    })}
+    <>
+      <h1>{user.name}</h1>
+      <h2>{team.name}</h2>
+      <ul>
+        {players.map((x, i) => {
+          return(
+            <li key={i}>{x.name}</li>
+          )
+        })}
+      </ul>
+    </>
   )
 }
