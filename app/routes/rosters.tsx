cc325ed8c1708264ccfb05ce32415fccce2c3b35
Brandon King <brandonscottking@gmail.com>
2022-09-12 20:58:37 -0500

    added button to set roster route

--- app/routes/rosters.tsx
+++ app/routes/rosters.tsx
@@ -1,5 +1,6 @@
 import { json } from "@remix-run/node";
 import { Outlet, useLoaderData } from "@remix-run/react";
+import { useState } from "react";
 import { getAllUsers } from "~/models/user.server";
 
 type LoaderData = {
@@ -12,20 +13,29 @@ export const loader = async () => {
   });
 };
 
+
 export default function Rosters() {
   const { owners } = useLoaderData() as unknown as LoaderData;
+
+  const [urlId, setUrlId] = useState(" ")
+  const changeUrl = (p: string) => {
+    window.location.replace('/rosters/'+ p)
+  }
+
   return (
     <div className="content">
       <h1>Team Roster</h1>
       <div id="user-filter">
-      <label>Choose Team</label>
-        <select>
-          {owners.map((x,i)=> {
-            return(
-              <option key={i}>{x.name}</option>
+        <label>Choose Team</label>
+        <select onChange={e => setUrlId(e.target.value)}>
+          <option value="">Select Team</option>
+          {owners.map((x, i) => {
+            return (
+              <option key={i} value={x.id}>{x.name}</option>
             )
           })}
         </select>
+        <button onClick={() => changeUrl(urlId)}>View</button>
       </div>
       <div id="roster-table">
         <Outlet />
